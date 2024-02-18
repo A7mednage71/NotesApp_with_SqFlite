109fdcb7314aa2b017374a4c890f3afd8ffe994a
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-02-18 13:00:39 +0200

    add home page file

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_proj/Home_page.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: homepage(),
+    );
+  }
+}
